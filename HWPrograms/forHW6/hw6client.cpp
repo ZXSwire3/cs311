6aeff6e7bcae2ce7fc25f4bc899e161dee810e2a
benfoltz <eggie.foltz@gmail.com>
2023-11-09 15:59:26 -0800

    completed client

--- HWPrograms/forHW6/hw6client.cpp
+++ HWPrograms/forHW6/hw6client.cpp
@@ -5,24 +5,47 @@
 
 // ================================================
 // HW#: HW6 dgraph client 
-// Name: **
-// File type:  **  (tester)
+// Name: Ben Foltz-Miranda
+// File type: client program (tester) hw6client.cpp
 // ================================================
 
-** // includes
+#include<iostream>
+#include "dgraph.h" // includes
 
-** // comments
-int main()
-{ ** 
+int main() {
+    //**
     //0.Declare dgraph object
+    dgraph G;
     //1.fillTable()
+    G.fillTable();
     //2.displayGraph()
+    G.displayGraph();
+
+    // Prompt the user to input a vertex name
+    string userInput;
+    cout << "Enter a vertex name or 'quit' to exit: ";
+    cin >> userInput;
     //while (the user does not want to stop)
-    //a.the user will specify which vertex     
-    //b.findOutDegree of the vertex and display the result
-    //b.findAdjacency of the vertex and display the result (see Hint)
-    //c.catch exception to display error mesg but do not exit
-    //end of while
+    while (userInput != "quit") {
+        //a.the user will specify which vertex
+        char vertex = toupper(userInput[0]);
+
+        try { //c.catch exception to display error mesg but do not exit
+            //b.findOutDegree of the vertex and display the result
+            cout << "Out degree of " << vertex << " is " << G.findOutDegree(vertex) << endl;
+            //b.findAdjacency of the vertex and display the result (see Hint)
+            slist l1;
+            l1 = G.findAdjacency(vertex);
+            // how do you display l1?  Hint: it is an slist from HW3.
+            l1.displayAll();
+        } catch (dgraph::BadVertex) {
+            cout << "n/a - Vertex not found" << endl;
+        }
+
+        // prompt user for next vertex
+        cout << "Enter a vertex name or 'quit' to exit: ";
+        cin >> userInput;
+    } // end of while
 }
 
 /*
