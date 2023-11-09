27fd6fcd0f2b9724f6f7389be6704eb0738ee45c
benfoltz <eggie.foltz@gmail.com>
2023-11-09 15:56:21 -0800

    reformatted code

--- HWPrograms/forHW6/dgraph.h
+++ HWPrograms/forHW6/dgraph.h
@@ -5,54 +5,54 @@
 
 // =======================================================
 // HW#: HW6 dgraph
-// Name: **
-// File Type: **
+// Name: Ben Foltz-Miranda
+// File Type: header file dgraph.h
 // ==================================================
 
 #include <iostream>
 #include "slist.h"
+
 using namespace std;
 
 //-----  globally setting up an alias ---------------------
 const int SIZE = 26;   // for the size of the graph table for A-Z
 
 // this will be in each GTable slot
-struct Gvertex
-{
-  char vertexName;      //the vertex Name
-  int outDegree;	//the Out degree
-  slist adjacentOnes;   //the adjecent vertices in an slist
-  int visit;            // This will hold the visit number in HW7 
+struct Gvertex {
+    char vertexName;      //the vertex Name
+    int outDegree;    //the Out degree
+    slist adjacentOnes;   //the adjecent vertices in an slist
+    int visit;            // This will hold the visit number in HW7
 };
 
-class dgraph
-{
+class dgraph {
 
 private:
-  Gvertex Gtable[SIZE];  // a table representing a dgraph
-  int  countUsed; // how many slots of the Gtable are actually used
+    Gvertex Gtable[SIZE];  // a table representing a dgraph
+    int countUsed; // how many slots of the Gtable are actually used
 
 public:
 
-  class BadVertex {};  // thrown when the vertex is not in the graph
- 
-  dgraph();    // initialize vertexName (blank) and visit numbers (0)
-               // initialize countUsed to be 0
-  ~dgraph();   // do we have to delete all nodes of slists in table??
-	       // Question: If we do not do this, will the llist destructor
-               // be called automatically??? Try it.
+    class BadVertex {
+    };  // thrown when the vertex is not in the graph
 
- // read from the input file and fill GTable
-  void fillTable();  
+    dgraph();    // initialize vertexName (blank) and visit numbers (0)
+    // initialize countUsed to be 0
+    ~dgraph();   // do we have to delete all nodes of slists in table??
+    // Question: If we do not do this, will the llist destructor
+    // be called automatically??? Try it.
 
- // displays in an easy to read table format 
-  void displayGraph(); 
+    // read from the input file and fill GTable
+    void fillTable();
 
-// returns the out degree of a given vertex - may throw BadVertex
-  int findOutDegree(char);  
-	
-// returns the adjacency list of a given vertex - may throw BadVertexx
-  slist findAdjacency(char);  
+    // displays in an easy to read table format
+    void displayGraph();
+
+    // returns the out degree of a given vertex - may throw BadVertex
+    int findOutDegree(char);
+
+    // returns the adjacency list of a given vertex - may throw BadVertexx
+    slist findAdjacency(char);
 
 };
 
