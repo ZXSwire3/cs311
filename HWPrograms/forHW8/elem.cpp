a693b6155823e81f8bc88f7eac00ab7297e75cfc
benfoltz <eggie.foltz@gmail.com>
2023-11-21 15:57:18 -0800

    fixed formatting for HW8

--- HWPrograms/forHW8/elem.cpp
+++ HWPrograms/forHW8/elem.cpp
@@ -8,47 +8,42 @@
 #include "elem.h"
 
 // blank element
-el_t::el_t()
-{
-  key = -1;  // cannot be 0 because 0 is a valid key
-  ** // initialize other parts to be blanks
+el_t::el_t() {
+    key = -1;  // cannot be 0 because 0 is a valid key
+    ** // initialize other parts to be blanks
 }
 
 // initializing constructor to create an el_t object 
-el_t::el_t(int akey, **)
-{
-  key = akey;
-  ** //  initialize other parts to be the arguments
+el_t::el_t(int akey,
+
+**) {
+key = akey;
+** //  initialize other parts to be the arguments
 }
 
 // ONLY the key part should be available to the user of this class
-int el_t::getkey()
-{
-  return key;
+int el_t::getkey() {
+    return key;
 }
 
 // ONLY the key part should be available to the user of this class 
-void el_t::setkey(int akey)
-{
-  key = akey;
-}
-  
-// overload == for search based on the key part only
-bool el_t::operator==(el_t OtherOne)
-{
-  if (this->key == OtherOne.key) return true; else return false;
+void el_t::setkey(int akey) {
+    key = akey;
 }
 
-bool el_t::operator!=(el_t OtherOne)
-{
-  if (this->key != OtherOne.key) return true; else return false;
+// overload == for search based on the key part only
+bool el_t::operator==(el_t OtherOne) {
+    if (this->key == OtherOne.key) return true; else return false;
+}
+
+bool el_t::operator!=(el_t OtherOne) {
+    if (this->key != OtherOne.key) return true; else return false;
 }
 
 // overload cout 
-ostream& operator<<(ostream& os, const el_t& E)  
-{  
-  os << E.key << " " << ** // display other parts of E in a nice way
-  return os;  
-}  
+ostream &operator<<(ostream &os, const el_t &E) {
+    os << E.key << " " << ** // display other parts of E in a nice way
+    return os;
+}
 // if you make it come out just like the input file, Saving to a file will
 // be easier
--- HWPrograms/forHW8/elem.h
+++ HWPrograms/forHW8/elem.h
@@ -10,40 +10,41 @@
 
 #include <iostream>
 #include <string>
+
 using namespace std;
 
-class el_t
-{
- private:  // these will change depending
-           // on the client needs
-  int key;      // key -- for EC, key has to change to string everywhere!!!
-  string name;  // customer name
-  //** add 2 more pieces of info here
+class el_t {
+private:  // these will change depending
+    // on the client needs
+    int key;      // key -- for EC, key has to change to string everywhere!!!
+    string name;  // customer name
+    //** add 2 more pieces of info here
 
- public:
+public:
 
-  el_t();  // to create a blank el_t object
-  el_t(int, string); // to create an initialized el_t object - useful for the client   ** add more arguments to create the object
+    el_t();  // to create a blank el_t object
+    el_t(int,
+         string); // to create an initialized el_t object - useful for the client   ** add more arguments to create the object
 
- // The following functions are available to
- // the htable class to be able to
- // use different el_t without modifying the code
+    // The following functions are available to
+    // the htable class to be able to
+    // use different el_t without modifying the code
 
-  int getkey(); // only the key part can be accessed by the user of this class.
+    int getkey(); // only the key part can be accessed by the user of this class.
 
-  void setkey(int); // only the key part can be accessed by the user of this class.
+    void setkey(int); // only the key part can be accessed by the user of this class.
 
 
-  // In linked list search,
-  //    == is used to compare node elements
-  // but what does it mean for this client to compare
-  // node elements?  
-  bool operator==(el_t);  // overload == for search
-  bool operator!=(el_t);  // overload != for search
+    // In linked list search,
+    //    == is used to compare node elements
+    // but what does it mean for this client to compare
+    // node elements?
+    bool operator==(el_t);  // overload == for search
+    bool operator!=(el_t);  // overload != for search
 
-  // This overloads cout for the el_t object
-  // This is a friend function since the receiver object is not el_t
-  friend ostream& operator<<(ostream&, const el_t&);
+    // This overloads cout for the el_t object
+    // This is a friend function since the receiver object is not el_t
+    friend ostream &operator<<(ostream &, const el_t &);
 
 
 };
